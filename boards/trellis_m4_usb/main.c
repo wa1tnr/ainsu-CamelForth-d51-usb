988e0a89c021b45f62cb4cf93d4fce8b209c5e8d
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-06 04:25:18 +0000

    neopixels for Trellis M4 - hooks On branch master 	modified:   main.c

--- boards/trellis_m4_usb/main.c
+++ boards/trellis_m4_usb/main.c
@@ -1,6 +1,8 @@
 #include <atmel_start.h>
 #include "atmel_start_pins.h"
 
+extern void npx_main(void);
+
 void delay_some(void) {
     for (volatile int i = 230001; i > 0; i--) {
     }
@@ -9,6 +11,7 @@ void delay_some(void) {
 void main_cf(void) {
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
+    npx_main();
 
     /* Replace with your application code */
     cdcd_acm_example();
