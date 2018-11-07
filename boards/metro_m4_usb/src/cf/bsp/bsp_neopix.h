64d796a2d14448f6d3c24701a8bd9aadc731e509
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-07 02:00:39 +0000

    external neopixels On branch ainsu-CamelForth-edits-bb- 	modified:   bsp_neopix.h

--- boards/metro_m4_usb/src/cf/bsp/bsp_neopix.h
+++ boards/metro_m4_usb/src/cf/bsp/bsp_neopix.h
@@ -1,7 +1,8 @@
 #ifndef LIB_BSP_NEOPIX_H
 #define LIB_BSP_NEOPIX_H
 
-// #include "PinMap.h"
+// EXTERN_NPX defined in driver_init.h
+#include "driver_init.h"
 
 // trellis M4 PA27
 // metro   M4 PB22
@@ -11,7 +12,19 @@
 #define PORTB 1
 
 #undef HAS_PORTB
-#define HAS_PORTB
+
+#ifdef EXTERN_NPX
+    #undef HAS_PORTB
+    // PA17 D12 Metro M4 Express
+    #define NEOPIX_PIN 17
+#endif
+
+#ifndef EXTERN_NPX
+    #define HAS_PORTB
+    // PB22 - internal NeoPixel - Metro M4 Express
+    // 22 + 32 = 54
+    #define NEOPIX_PIN 54
+#endif
 
 extern void neopix_show_400k(uint32_t pin, uint8_t *pixels, uint16_t numBytes);
 extern void neopix_show_800k(uint32_t pin, uint8_t *pixels, uint16_t numBytes);
