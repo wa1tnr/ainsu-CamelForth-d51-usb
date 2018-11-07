6a9e7c32765653c38f31d58f687eb24ca93ed7f8
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-07 00:23:44 +0000

    Simple entry point for NeoPixel demo On branch ainsu-CamelForth-edits-aa- 	modified:   main.c

--- boards/metro_m4_usb/main.c
+++ boards/metro_m4_usb/main.c
@@ -1,7 +1,8 @@
 #include <atmel_start.h>
 #include "atmel_start_pins.h"
-// #include "main_app.inc"
 #include "pins.h"               // local
+
+extern void npx_main(void);
 
 void delay_some(void) {
     for (volatile int i = 230001; i > 0; i--) {
@@ -32,6 +33,7 @@ void main_cf(void) {
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
     pins_setup();
+    npx_main();
 
     /* Replace with your application code */
     // blinks();
