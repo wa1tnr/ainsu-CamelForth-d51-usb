dbd8d25a306cdee42cf24068f4192df6562065ad
wa1tnr <wa1tnr@users.noreply.github.com>
2018-11-07 01:23:10 +0000

    Internal NPX support PB22 On branch ainsu-CamelForth-edits-bb- 	modified:   bsp_neopix.h

--- boards/metro_m4_usb/src/cf/bsp/bsp_neopix.h
+++ boards/metro_m4_usb/src/cf/bsp/bsp_neopix.h
@@ -10,7 +10,8 @@
 #define PORTA 0
 #define PORTB 1
 
-// #define HAS_PORTB
+#undef HAS_PORTB
+#define HAS_PORTB
 
 extern void neopix_show_400k(uint32_t pin, uint8_t *pixels, uint16_t numBytes);
 extern void neopix_show_800k(uint32_t pin, uint8_t *pixels, uint16_t numBytes);
